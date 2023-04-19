ddf774358bfc20d586fdfb1198dca3895342d278
Felipe-Dev <felipe-developer@outlook.com>
2023-04-19 16:20:11 -0300

    Otimização do código

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function calcular() {
-    let a = document.getElementById('vlA').value
-    let b = document.getElementById('vlB').value
-    let c = document.getElementById('vlC').value
+  let a = document.getElementById("vlA").value;
+  let b = document.getElementById("vlB").value;
+  let c = document.getElementById("vlC").value;
 
-    // CALCULO DE DELTA
-     let delta = b**2 - 4*a*c 
-     
-     //  CALCULO DE BHASKARA
-     let x1 = (-b + Math.sqrt(delta))/(2*a)
-     let x2 = (-b - Math.sqrt(delta))/(2*a)
-     if (a = 0) {
-        alert('O VALOR DE <strong>A</strong> NÃO PODE SER IGUAL A 0 ')
-        return
-     }
-     if (x1 == NaN && x2 == NaN) {
-        alert('A EQUAÇÃO NÃO POSSUI RAÍZES ')
-     }
-    
-     console.log(delta)
-     console.log(x1, x2)
-     document.getElementById('x1').innerHTML = x1.toFixed(2)
-     document.getElementById('x2').innerHTML = x2.toFixed(2)
-       document.getElementById('delta').innerHTML = delta
+  // CALCULO DE DELTA
+  let delta = b ** 2 - 4 * a * c;
 
+  //  CALCULO DE BHASKARA
+  let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+  let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+  if (a == 0) {
+    alert("Os três coeficientes não formam uma equação do segundo grau ");
+    return;
+  }
+  if (delta < 0) {
+    document.getElementById("delta").innerHTML = "Delta:" + delta;
+    document.querySelector(".result-raiz").innerHTML =
+      "A equação não possui raízes reais";
+    return;
+  }
+  document.getElementById("delta").innerHTML = "Delta:" + delta;
+  document.getElementById("x1").innerHTML = "x' : " + x1.toFixed(2);
+  document.getElementById("x2").innerHTML = 'x" : ' + x2.toFixed(2);
+  return;
 }
